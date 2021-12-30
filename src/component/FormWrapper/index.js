0349b95a6410717298e707fb5f3c984e4fa35f88
Shrivishnu <srivishnu31@gmail.com>
2021-12-30 22:50:06 +0530

    Added API

--- src/component/FormWrapper/index.js
+++ src/component/FormWrapper/index.js
@@ -1,26 +1,59 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./FormWrapper.css";
+const axios = require('axios');
 
 const FormWrapper = () => {
   const [state, setState] = useState({
-    textId: null,
+    textId: 0,
     text: "",
     addCount: 0,
     updateCount: 0,
   });
 
-  const addHandler = useCallback(() => {
-    setState((prevState) => ({ ...prevState, text: "" }));
-  }, []);
+  const apiDomain = process.env.BACKEND_URI
+
+  useEffect(()=>{
+    axios.get(`${apiDomain}/fetch-count`).then(({ data })=>{
+        setState((prevState)=>({
+            ...prevState,
+            textId: data.addCount + 1,
+            addCount: data.addCount,
+            updateCount: data.updateCount,
+          }))        
+    }).catch(()=>{
+        setState({
+            textId: 0,
+            text: "",
+            addCount: 0,
+            updateCount: 0,
+          })
+    })
+  },[apiDomain])
+
+  const addHandler = useCallback(async () => {
+      try{
+         const axiosData = await axios.post(`${apiDomain}/add-data`,{ text: state.text })
+         const { data: { textId } } = axiosData
+         setState((prevState) => ({ ...prevState, textId, addCount: prevState.addCount+1 }))
+      }catch(error){
+        setState((prevState) => ({ ...prevState }))
+      }
+  }, [apiDomain, state.text]);
   
-  const updateHandler = useCallback(() => {
-    setState((prevState) => ({ ...prevState, text: "" }));
-  }, []);
+  const updateHandler = useCallback(async () => {
+    try{
+       await axios.patch(`${apiDomain}/update-data`,{ textId: state.textId, text: state.text })
+       setState((prevState) => ({ ...prevState, updateCount: prevState.updateCount+1 }))
+    }catch(error){
+      setState((prevState) => ({ ...prevState }))
+    }
+}, [apiDomain, state.textId, state.text]);
 
   const textUpdateHandler = useCallback((event) => {
     const value = event.target.value;
     setState((prevState) => ({ ...prevState, text: value }));
   }, []);
+
   return (
     <div className="form-wrapper">
       <h1>Content Form</h1>
